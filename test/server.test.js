3e5deadbf956949db3b46c46d5871ee715bf6400
alberto <curribifine@gmail.com>
2025-04-21 15:28:34 -0500

    commit

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,21 @@
 const request = require('supertest');
 const app = require('../server');
 
+let server;
+
+// Inicia el servidor antes de ejecutar las pruebas
+beforeAll(() => {
+    server = app.listen(3000, () => {
+        console.log('Servidor corriendo en http://localhost:3000');
+    });
+});
+
+
+// Cierra el servidor después de ejecutar las pruebas
+afterAll((done) => {
+    server.close(done); // Esto asegura que Jest cierre el proceso correctamente
+});
+
 describe('GET /api/message', () => {
     it('Debería devolver un mensaje', async () => {
         const res = await request(app).get('/api/message');
