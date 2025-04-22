53e2ec4466f09d0299af839ef3d85d52d5a5a94c
alberto <curribifine@gmail.com>
2025-04-22 06:58:39 -0500

    comit

--- test/server.test.js
+++ test/server.test.js
@@ -3,17 +3,17 @@ const app = require('../server');
 
 let server;
 
-// Inicia el servidor antes de ejecutar las pruebas
-beforeAll(() => {
+// Inicia el servidor antes de ejecutar las pruebas y espera a que esté listo
+beforeAll(async () => {
+    await new Promise(resolve => setTimeout(resolve, 5000)); // Esperar 5 segundos antes de ejecutar pruebas
     server = app.listen(3000, () => {
         console.log('Servidor corriendo en http://localhost:3000');
     });
 });
 
-
 // Cierra el servidor después de ejecutar las pruebas
 afterAll((done) => {
-    server.close(done); // Esto asegura que Jest cierre el proceso correctamente
+    server.close(done);
 });
 
 describe('GET /api/message', () => {
